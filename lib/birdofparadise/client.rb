8425cd7c5b406ab42c876ea50b2983ad6ce3a4cc
Colin Ewen <colin@draecas.com>
2014-12-13 16:59:27 -0500

    Searching

--- lib/birdofparadise/client.rb
+++ lib/birdofparadise/client.rb
@@ -1,4 +1,16 @@
+require 'httparty'
 module Birdofparadise
   class Client
+    REGISTRY_URL = "http://bower.herokuapp.com/packages"
+    attr_reader :index
+
+    def get_index
+      @index ||= HTTParty.get(REGISTRY_URL).parsed_response
+    end
+
+    def search(query)
+      get_index if @index.nil?
+      @index.find_all { |package| package["name"].match query }
+    end
   end
 end
